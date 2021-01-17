6de0c0b20dbca617cfa84b9ba8271d0b1d6c6b56
Ezequiel Leonardo Castaño <elcg@gmx.com>
2021-01-17 13:35:53 -0300

    Fix Black Screen when Rating Recommendation

--- src/components/Recommendations/Recommendations.vue
+++ src/components/Recommendations/Recommendations.vue
@@ -10,7 +10,7 @@
       </div>
 
 
-      <Modal message="Keep Rating to Unlock More Recommendations" :callback="resetAndRate"></Modal>
+      <Modal message="Keep Rating to Unlock More Recommendations" :alwaysvisible="true" :callback="resetAndRate" v-if="getMovie.movieId === -1"></Modal>
 
       <div class="recommendations-content" v-if="getMovie.movieId !== -1">
 
